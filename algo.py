8f99953d9556335d09734abbfc600187f953e2d1
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 17:23:22 -0600

    sort the numbers array

--- algo.py
+++ algo.py
@@ -3,5 +3,7 @@
 #################################################
 
 def sum_two_smallest_numbers(numbers):
-    #your code here
-    pass
+    numbers.sort();
+    print ("Numbers:", numbers)
+
+sum_two_smallest_numbers([19, 5, 42, 2, 77])
