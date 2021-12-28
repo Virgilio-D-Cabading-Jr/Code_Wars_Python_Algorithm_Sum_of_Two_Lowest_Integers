a2dc04cc5a1ffde2ea9d3f0878c3e5c2efaa44fc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 17:37:43 -0600

    add test 2

--- algo.py
+++ algo.py
@@ -7,3 +7,11 @@ def sum_two_smallest_numbers(numbers):
     return numbers[0] + numbers[1]
 
 print("sum_two_smallest_numbers([19, 5, 42, 2, 77]):", sum_two_smallest_numbers([19, 5, 42, 2, 77]))
+
+print("sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]):", sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]))
+
+# print("sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]):", sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]))
+
+# print("sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]):", sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]))
+
+# print("sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]):", sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]))
